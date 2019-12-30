db9e0a93b179bdcbcfe076ca8fd7f4367aee4ad2
vuong ngoc hieu <vuongngochieu030299@gmail.com>
2019-12-30 06:11:31 -0800

    lan 5

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.main-carousel').slick({
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        autoplaySpeed: 2000
+    });
+  });
